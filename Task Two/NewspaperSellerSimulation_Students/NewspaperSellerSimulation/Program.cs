76a04985639db87322c3ff77574179e66aa0c391
maryamabubkr <47347291+maryamabubkr@users.noreply.github.com>
2021-05-11 14:28:57 +0200

    Add files via upload
    Program.cs uploaded

---
+++ Task Two/NewspaperSellerSimulation_Students/NewspaperSellerSimulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewspaperSellerTesting;
+
+namespace NewspaperSellerSimulation
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //maryam btgarb
+            NewspaperSellerModels.SimulationSystem s = new NewspaperSellerModels.SimulationSystem();
+            s.ReadFile();
+            //Application.Run(new Form1());
+        }
+    }
+}
