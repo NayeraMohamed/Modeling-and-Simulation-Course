13db7af8e5da192c52422bafef12ee96f1d06cbf
Reem Osama <32741801+ReemOsama99@users.noreply.github.com>
2021-05-31 21:49:50 +0200

    Add files via upload

---
+++ Task 3-inventorysimulation/InventorySimulation/Program.cs
@@ -0,0 +1,33 @@
+﻿using InventoryModels;
+using InventoryTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventorySimulation
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            SimulationSystem simulationSystem = new SimulationSystem();
+            simulationSystem.ReadFile();
+
+            Calculations calculations = new Calculations(simulationSystem);
+            simulationSystem = calculations.calculate();
+
+            string result = TestingManager.Test(simulationSystem, Constants.FileNames.TestCase1); 
+            MessageBox.Show(result);
+            Application.Run(new Form1(calculations.calculate()));
+        }
+    }
+}
